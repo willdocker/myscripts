6adc0ef930f0d9c0f42bc3215fb6cf4963cfb243
william <will.desenvolvimento@gmail.com>
2017-02-09 16:43:55 -0200

    0.1

---
+++ install_jenkins.sh
@@ -0,0 +1,6 @@
+yum -y install java
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum -y install jenkins
+systemctl start jenkins.service
+systemctl status jenkins.service
