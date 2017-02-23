f2d19875ad26f2d7a58e97c9b0a7cda7afb7af36
willdocker <will.desenvolvimento@gmail.com>
2017-02-23 16:12:20 -0300

    Update default.sh

--- default.sh
+++ default.sh
@@ -4,5 +4,6 @@ systemctl stop firewalld
 systemctl disable firewalld
 yum -y install openldap-clients nss-pam-ldapd
 yum install net-tools vim authconfig-tui nss-pam-ldapd.x86_64 -y
+#authconfig --enableforcelegacy --enableldap --enableldapauth --enablemd5 --enableshadow --ldapserver="" --ldapbasedn="dc=zp,dc=net,dc=br"  --update
 yum -y update
 
