4a2b10ceb1d6e895a9c10438b1600b11c3f18d80
willdocker <will.desenvolvimento@gmail.com>
2017-03-08 15:00:33 -0300

    Update install_zabbix.sh

--- install_zabbix.sh
+++ install_zabbix.sh
@@ -7,17 +7,17 @@ cd /opt/zabbix-repo
 rpm -Uvh http://repo.zabbix.com/zabbix/3.0/rhel/7/x86_64/zabbix-release-3.0-1.el7.noarch.rpm
 yum -y remove zabbix-agent
 yum update -y && yum install zabbix-agent -y
-echo "Ajustando arquivo de configuração"
-sed -i 's/127.0.0.1/192.168.20.53/g' /etc/zabbix/zabbix_agentd.conf
+echo "Ajustando arquivo de configuraÃ§Ã£o"
+sed -i 's/127.0.0.1/10.100.2.51/g' /etc/zabbix/zabbix_agentd.conf
 sed -i '136s/^/#/' /etc/zabbix/zabbix_agentd.conf
 sed -i "147s/.*/$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
-echo "Iniciando Serviço"
+echo "Iniciando ServiÃ§o"
 systemctl enable zabbix-agent.service
 systemctl start zabbix-agent.service
-echo "Verificando se os serviço esta rondando"
+echo "Verificando se os serviÃ§o esta rondando"
 #cat /var/log/zabbix/zabbix_agentd.log
 ps -aux | grep zabbix
-echo "Instalação encerrada!"
+echo "InstalaÃ§Ã£o encerrada!"
 exit
 
 
